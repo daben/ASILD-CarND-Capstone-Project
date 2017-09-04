813fa6b58a441ac9193c449d7eec87d8910d61f9
Pierre Merriaux <pierre.merriaux@gmail.com>
2017-09-04 08:36:51 +0200

    dbw node frequency 10->50Hz

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -36,7 +36,7 @@ class DBWNode(object):
     def __init__(self):
         rospy.init_node('dbw_node')
 
-        self.sampleFrequency = 10
+        self.sampleFrequency = 50
 
         vehicle_mass = rospy.get_param('~vehicle_mass', 1736.35)
         fuel_capacity = rospy.get_param('~fuel_capacity', 13.5)
