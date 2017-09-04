a819d4358dee91f4c475f2bf2411038103bfa911
Pierre Merriaux <pierre.merriaux@gmail.com>
2017-09-04 14:32:02 +0200

    Fix #1 WaypointUpdater emits waypoints ignoring traffic lights
    Squashed commits:
    [b54b540] Set offset to closed waypoint index to 1 (+5 squashed commits)
    [4f8afe8] Clean comments and white spaces
    [328ea8d] correct problems in waypoint updater, if len(waypoint)<LOOKAHEAD_WPS
    [3e8ba73] many modifictions for merge with david
    [acaacef] first version of waypoint updater, solve case #1

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -2,7 +2,11 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
+
+
 from styx_msgs.msg import Lane, Waypoint
+import sys
+import numpy as np
 
 import math
 
@@ -26,27 +30,57 @@ LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this n
 
 class WaypointUpdater(object):
     def __init__(self):
-        rospy.init_node('waypoint_updater')
+        rospy.init_node('waypoint_updater', log_level=rospy.DEBUG)
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+        self.base_wp_sub = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        # waypoints base list
+        self.wpsBase = None
+        self.wpsPositions = None #numpy array waypoint positions [X,Y], to optimize time computation
+
+        # Number added to the closest waypoint index, to be sure to be in front and not in back.
+        # with the closest algorithm, the waypoint found could be in front or in back of the car
+        self.numberInFrontOfClosestWaypointIndex = 1
 
         rospy.spin()
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        if msg is None or self.wpsPositions is None:
+            return
+
+        minDistance, minDistanceIndex = self.closestWp2PoseNumpy(msg.pose)
+        wpOut = self.selectWP(self.wpsBase, minDistanceIndex)
+        lane = Lane();
+        lane.header.frame_id = '/world'
+        lane.header.stamp = rospy.Time(0)
+        lane.waypoints = wpOut
+
+        self.final_waypoints_pub.publish(lane)
+        rospy.logdebug("final wp index:%d, dist:%.2f; x:%.2f, y:%.2f; cx:%.2f, cy:%.2f",
+                       minDistanceIndex, minDistance,
+                       self.wpsBase[minDistanceIndex].pose.pose.position.x,
+                       self.wpsBase[minDistanceIndex].pose.pose.position.y,
+                       msg.pose.position.x,
+                       msg.pose.position.y)
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        if(waypoints is None):
+            return
+        self.wpsBase = waypoints.waypoints; # store waypoints as list
+        # cancel it, bad idea !
+        # self.base_wp_sub.unregister() # unsubcribe after the first data receive. It is always the same data
+
+        # copy each waypoint position in numpy array
+        self.wpsPositions = np.zeros((len(self.wpsBase),2), dtype=np.double)
+        for i in range(0, len(self.wpsBase)):
+            self.wpsPositions[i, 0] = waypoints.waypoints[i].pose.pose.position.x
+            self.wpsPositions[i, 1] = waypoints.waypoints[i].pose.pose.position.y
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -69,6 +103,27 @@ class WaypointUpdater(object):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
         return dist
+
+    # todo, start form last position --> maybe to usefull with numpy computing
+    def closestWp2PoseNumpy(self, pose):
+        wpDeltaX = self.wpsPositions[:, 0] - pose.position.x
+        wpDeltaY = self.wpsPositions[:, 1] - pose.position.y
+        deltaSquareDistance = (wpDeltaX*wpDeltaX)+(wpDeltaY*wpDeltaY)
+
+        minDistanceIndex = deltaSquareDistance.argmin()
+        minDistance = deltaSquareDistance[minDistanceIndex]
+        return minDistance, minDistanceIndex
+
+    def selectWP(self,waypoints,indexClosest):
+        idx = (indexClosest + self.numberInFrontOfClosestWaypointIndex) % len(waypoints)
+
+        wpOut = waypoints[idx:idx+LOOKAHEAD_WPS]
+        # while to avoid problem if len(waypoint)<LOOKAHEAD_WPS
+        # make assumption than track looped
+        while len(wpOut) < LOOKAHEAD_WPS:
+            wpOut.extend(waypoints[:LOOKAHEAD_WPS - len(wpOut)])
+
+        return wpOut
 
 
 if __name__ == '__main__':
