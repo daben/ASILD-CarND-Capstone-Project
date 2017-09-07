36e5b6d99e2d1adbeb770fface42374c9a4ba033
David Moriano <dabengrc@gmail.com>
2017-09-07 13:02:19 +0200

    Fix #15 - corrects manipulation of velocity units in waypoint_loader.py

--- ros/src/waypoint_loader/waypoint_loader.py
+++ ros/src/waypoint_loader/waypoint_loader.py
@@ -51,7 +51,7 @@ class WaypointLoader(object):
                 p.pose.pose.position.z = float(wp['z'])
                 q = self.quaternion_from_yaw(float(wp['yaw']))
                 p.pose.pose.orientation = Quaternion(*q)
-                p.twist.twist.linear.x = float(self.velocity*0.27778)
+                p.twist.twist.linear.x = self.velocity*0.44704
 
                 waypoints.append(p)
         return self.decelerate(waypoints)
@@ -65,7 +65,7 @@ class WaypointLoader(object):
         last.twist.twist.linear.x = 0.
         for wp in waypoints[:-1][::-1]:
             dist = self.distance(wp.pose.pose.position, last.pose.pose.position)
-            vel = math.sqrt(2 * MAX_DECEL * dist) * 3.6
+            vel = math.sqrt(2 * MAX_DECEL * dist)
             if vel < 1.:
                 vel = 0.
             wp.twist.twist.linear.x = min(vel, wp.twist.twist.linear.x)
