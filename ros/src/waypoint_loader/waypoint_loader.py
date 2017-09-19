1be7d759b8db812adfa9673e7e8d2781c03bfc8d
Caleb Kirksey <ckirksey3@gmail.com>
2017-09-19 15:53:42 -0700

    Fix waypoint loader to publish the base waypoints only once as peer udacity/CarND-Capstone@5157f0cec0548fcb47a053cf8006ff6727dabd10

--- ros/src/waypoint_loader/waypoint_loader.py
+++ ros/src/waypoint_loader/waypoint_loader.py
@@ -20,7 +20,7 @@ class WaypointLoader(object):
     def __init__(self):
         rospy.init_node('waypoint_loader', log_level=rospy.DEBUG)
 
-        self.pub = rospy.Publisher('/base_waypoints', Lane, queue_size=1)
+        self.pub = rospy.Publisher('/base_waypoints', Lane, queue_size=1, latch=True)
 
         self.velocity = rospy.get_param('~velocity')
         self.new_waypoint_loader(rospy.get_param('~path'))
@@ -72,16 +72,11 @@ class WaypointLoader(object):
         return waypoints
 
     def publish(self, waypoints):
-        publish_frequency = rospy.get_param("~publish_frequency", 40)  # 40 Hz by default
-        rospy.logdebug("waypoint loader publishing at %.1f Hz", publish_frequency)
-        rate = rospy.Rate(publish_frequency)
-        while not rospy.is_shutdown():
-            lane = Lane()
-            lane.header.frame_id = '/world'
-            lane.header.stamp = rospy.Time(0)
-            lane.waypoints = waypoints
-            self.pub.publish(lane)
-            rate.sleep()
+        lane = Lane()
+        lane.header.frame_id = '/world'
+        lane.header.stamp = rospy.Time(0)
+        lane.waypoints = waypoints
+        self.pub.publish(lane)
 
 
 if __name__ == '__main__':
